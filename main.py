eaedc1db58319deef66f59ea6e925c33ad23dc88
AlexVasilkov <alex.wasilkow@gmai.com>
2023-04-12 15:57:05 +0400

    first track

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
+payload = {'name':'User'}
+response = requests.get('https://playground.learnqa.ru/api/hello',params=payload)
 
-response = requests.get('https://playground.learnqa.ru/api/get_text')
 print(response.text)
 
