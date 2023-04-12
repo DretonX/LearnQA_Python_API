235d9b730b025a13458153bd3a1e4775a7f3d20a
AlexVasilkov <alex.wasilkow@gmai.com>
2023-04-12 18:22:29 +0400

    first track

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 
 import requests
 
-headers = {'some_header':'123'}
-response = requests.get('https://playground.learnqa.ru/api/show_all_headers',headers = headers)
+
+payload = {'login':'secret_login','password':'secret_pass'}
+response = requests.post('https://playground.learnqa.ru/api/get_auth_cookie', data=payload)
 print(response.text)
+print(response.status_code)
+print(dict(response.cookies))
 print(response.headers)
-
-
