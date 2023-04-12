9492244dcb2ef9a1e9b337940840edaa1a92a181
AlexVasilkov <alex.wasilkow@gmai.com>
2023-04-12 18:13:02 +0400

    first track

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 
 import requests
 
-response = requests.post('https://playground.learnqa.ru/api/get_301', allow_redirects=True)
-first_response = response.history[0]
-second_response = response
-print(first_response.url)
-print(second_response.url)
+headers = {'some_header':'123'}
+response = requests.get('https://playground.learnqa.ru/api/show_all_headers',headers = headers)
+print(response.text)
+print(response.headers)
 
 
