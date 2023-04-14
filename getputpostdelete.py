092df23953b117d5877cef6394f69395d6849c03
AlexVasilkov <alex.wasilkow@gmai.com>
2023-04-14 16:55:47 +0400

    first track

--- getputpostdelete.py
+++ getputpostdelete.py
@@ -1,5 +1,13 @@
 import requests
 
+response = requests.delete('https://playground.learnqa.ru/ajax/api/compare_query_type')
+
+print(f'without method  - {response.text}')
+
+response = requests.head('https://playground.learnqa.ru/ajax/api/compare_query_type')
+
+print(f'with request HEAD - {response.text}')
+
 payload = {'method': 'GET'}
 response = requests.get('https://playground.learnqa.ru/ajax/api/compare_query_type', params=payload)
 
@@ -19,3 +27,19 @@ payload = {'method': 'DELETE'}
 response = requests.delete('https://playground.learnqa.ru/ajax/api/compare_query_type', data=payload)
 
 print(response.text)
+
+
+
+list_req = ['POST', 'GET', 'DELETE', 'PUT']
+url = 'https://playground.learnqa.ru/ajax/api/compare_query_type'
+
+methods = {'POST': requests.post, 'GET': requests.get, 'DELETE': requests.delete, 'PUT': requests.put}
+
+for method in list_req:
+    if method == 'GET':
+        payload = {'method': method}
+        response = methods[method](url, params=payload)  # передаем параметры через params для GET-запроса
+    else:
+        payload = {'method': method}
+        response = methods[method](url, data=payload)  # передаем параметры через data для остальных запросов
+    print(f'{response.text} - {method}')
