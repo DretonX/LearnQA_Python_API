bdb49730f737b81e5f73004c2416c9218b97e42e
AlexVasilkov <alex.wasilkow@gmai.com>
2023-04-20 15:03:05 +0400

    first track

--- first_api.py
+++ first_api.py
@@ -1,9 +1,16 @@
+import pytest
 import requests
 
 class TestFirstApi:
-    def test_hello_call(self):
+    names = [
+        ('Vitalii'),
+        ('Arsenii'),
+        ('')
+    ]
+
+    @pytest.mark.parametrize('name', names)
+    def test_hello_call(self, name):
         url = "https://playground.learnqa.ru/api/hello"
-        name = 'Vitalii'
         data = {'name': name}
         response = requests.get(url, params=data)
 
@@ -11,7 +18,11 @@ class TestFirstApi:
 
         response_dict = response.json()
         assert 'answer' in response_dict, "There is no field 'answer' in the response"
+        if len(name)==0:
+            expected_response_text = "Hello, someone"
+        else:
+            expected_response_text = f"Hello, {name}"
 
-        expected_response_text = f"Hello, {name}"
+
         actual_response_text = response_dict['answer']
         assert actual_response_text == expected_response_text, f"Actual text in the response is not correct"
