6955675ca8ff6590e887ba84ae6aa795ec0d50d1
AlexVasilkov <alex.wasilkow@gmai.com>
2023-04-26 15:55:53 +0400

    First track

--- tests/test_user_register.py
+++ tests/test_user_register.py
@@ -1,3 +1,5 @@
+import pytest
+
 from lib.my_requests import MyRequests
 from lib.base_case import BaseCase
 from lib.assertions import Assertions
@@ -22,3 +24,40 @@ class TestUserRegister(BaseCase):
         Assertions.assert_code_status(response, 400)
         assert response.content.decode(
             "utf-8") == f"Users with email '{email}' already exists", f"Unexpected response content {response.content}"
+
+    def test_create_user_with_invalid_email(self):
+        email = 'vinkotovexample.com'
+        data = self.prepare_registration_data(email)
+
+        response = MyRequests.post('/user/', data=data)
+
+        Assertions.assert_code_status(response, 400)
+        assert response.content.decode("utf-8") == "Invalid email format", f"Unexpected response content {response.content}"
+
+    @pytest.mark.parametrize('missing_field', ['password', 'username', 'firstName', 'lastName', 'email'])
+    def test_create_user_without_field(self, missing_field):
+        data = self.prepare_registration_data()
+        del data[missing_field]
+
+        response = MyRequests.post('/user/', data=data)
+
+        Assertions.assert_code_status(response, 400)
+        assert response.content.decode("utf-8") == f"The following required params are missed: {missing_field}", f"Unexpected response content {response.content}"
+
+    def test_create_user_with_short_name(self):
+        data = self.prepare_registration_data()
+        data['firstName'] = 'a'
+
+        response = MyRequests.post('/user/', data=data)
+
+        Assertions.assert_code_status(response, 400)
+        assert response.content.decode("utf-8") == "The value of 'firstName' field is too short", f"Unexpected response content {response.content}"
+
+    def test_create_user_with_long_name(self):
+        data = self.prepare_registration_data()
+        data['firstName'] = 'a' * 251
+
+        response = MyRequests.post('/user/', data=data)
+
+        Assertions.assert_code_status(response, 400)
+        assert response.content.decode("utf-8") == "The value of 'firstName' field is too long", f"Unexpected response content {response.content}"
